75a82ab5e1953bbabb0da756dc21611980125328
orhanince <ince.orhan@yandex.com>
2024-07-28 17:02:35 +0300

    Add colly to scrape

--- src/router/route.go
+++ src/router/route.go
@@ -11,11 +11,15 @@ func SetupRoutes(app *fiber.App) {
 	// Middleware
 	api := app.Group("/api", logger.New())
 	user := app.Group("/api/user", logger.New())
-
+	scrape := app.Group("/api/scrape", logger.New())
 	// Routes
 	api.Get("/", handler.Home)
+
 	// User routes
 	user.Get("/", handler.Check)
 	user.Get("/test", handler.Test)
+
+	// Scrape
+	scrape.Get("/", handler.Scrape)
 
 }
